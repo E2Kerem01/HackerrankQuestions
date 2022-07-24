37089d5a73d79de292fbaf5c0f3ce792cb371f46
Kerem Metin <k.metin01@gmail.com>
2022-07-25 00:50:10 +0300

    files

---
+++ src/JavaStringTokens.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+
+
+        s = s.trim();
+
+
+        if (s.length()==0){
+            System.out.println(0);
+        }else {
+            String sList[] = s.split("['!?,._@ ]+");
+            System.out.println(sList.length);
+            for (int i = 0; i < sList.length ; i++){
+
+                System.out.println(sList[i]);
+
+            }
+        }
+
+    }
+}
