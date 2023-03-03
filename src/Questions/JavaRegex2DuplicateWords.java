62e9954a3e1d875a8afb86db9500578ad1730511
E2Kerem01 <k.metin01@gmail.com>
2023-03-03 23:43:27 +0300

    JavaRegex

---
+++ src/Questions/JavaRegex2DuplicateWords.java
@@ -0,0 +1,38 @@
+package Questions;
+
+
+import java.util.*;
+
+public class JavaRegex2DuplicateWords {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int stringNumber = scan.nextInt();
+        scan.nextLine();
+
+        for(int i = 0; i < stringNumber; i++){
+            String sentence = scan.nextLine();
+            myRegex(sentence);
+
+        }
+    }
+
+    static void myRegex(String a){
+        List<String> b = new ArrayList<>(Arrays.asList(a.split(" ")));
+        int limit = b.size() - 1;
+        for (int k = 0; k <= limit; k++){
+            for (int j = k + 1; j <= limit; j++){
+                if (b.get(k).toLowerCase().equals(b.get(j).toLowerCase())){
+                    b.remove(j);
+                    limit--;
+                    j--;
+                }
+            }
+
+        }String joinedString = String.join(" ", b);
+        System.out.println(joinedString);
+
+    }
+}
---
+++ src/Questions/ScribbleArea.java
@@ -0,0 +1,33 @@
+package Questions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScribbleArea {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int stringNumber = scan.nextInt();
+        String result = null;
+        scan.nextLine();
+
+        for(int i = 0; i < stringNumber; i++){
+            String sentence = scan.nextLine();
+            List<String> b = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+            int limit = b.size() - 1;
+            for (int k = 0; k <= limit; k++){
+                for (int j = k + 1; j <= limit; j++){
+                    if (b.get(k).equalsIgnoreCase(b.get(j))){
+                        b.remove(j);
+                        limit--;
+                        j--;
+                    }
+                }
+            }result = String.join(" ", b);
+            System.out.print(result);
+        }scan.close();
+    }
+}
