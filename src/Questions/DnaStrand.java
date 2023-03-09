010fb13f640ace3c050603d34fae44db17dbca53
E2Kerem01 <k.metin01@gmail.com>
2023-03-09 13:54:35 +0300

    JavaRegex

---
+++ src/Questions/DnaStrand.java
@@ -0,0 +1,28 @@
+package Questions;
+
+import java.util.Scanner;
+
+public class DnaStrand {
+
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        String s = scan.nextLine();
+        StringBuilder sbf = new StringBuilder(s);
+        sbf.reverse();
+
+        for (int i = 0; i < s.length(); i++){
+
+            if (sbf.charAt(i)=='G'){
+                sbf.append('C');
+            }if (sbf.charAt(i)=='C'){
+                sbf.append('G');
+            }if (sbf.charAt(i)=='T'){
+                sbf.append('A');
+            }if (sbf.charAt(i)=='A'){
+                sbf.append('T');
+            }
+
+        }sbf.delete(0,s.length());
+        System.out.println(sbf);
+    }
+}
---
+++ src/Questions/FizzBuzz.java
@@ -0,0 +1,26 @@
+package Questions;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan  = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        for (int i = 1; i < n+1; i++){
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("FizzBuzz");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+}
